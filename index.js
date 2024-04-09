d6d4f7a2c7a721e23d482c370daba960d2829c96
Rohit Kumar Lohiya <lohiyarohitmau@gmail.com>
2024-04-09 20:00:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     res.send(html);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
